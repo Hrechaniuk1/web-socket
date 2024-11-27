5aff7f7d4abe481cf7b36314609325c75997244a
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-11-27 13:46:37 +0300

    web socket server start

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { WebSocketServer } from 'ws';
+import http from 'http'
 
 // custom imports
 import { env } from './helpers/env.js';
@@ -11,10 +13,14 @@ import router from './routes/index.js';
 
 // support code
 const PORT = Number(env('PORT'));
+// const PORTWS = Number(env('PORTWS'))
 
 // server code
 
 export const setupServer = async () => {
+
+    // express
+
     const app = express();
 
     app.use(cors());
@@ -27,7 +33,39 @@ export const setupServer = async () => {
 
     app.use(errorHandler);
 
+    // main server
+    const server = http.createServer(app);
+
+    // wss
+    const wss = new WebSocketServer({server})
+
+    wss.on('connection', (ws) => {
+        console.log('New WebSocket client connected');
+
+        // Обработка полученных сообщений
+        ws.on('message', (message) => {
+            console.log('Received:', message);
+
+            // Рассылаем сообщение всем клиентам
+            wss.clients.forEach((client) => {
+                if (client.readyState === ws.OPEN) {
+                    client.send(message);
+                }
+            });
+        });
+
+        // Обработка отключений
+        ws.on('close', () => {
+            console.log('WebSocket client disconnected');
+        });
+    });
+
+    // start server
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
+});
+    server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 };
